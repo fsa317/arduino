6a4f4cd0458abfcf9fb883b79ff3ac99dedd551e
Frank Apap <tvfoodmaps@gmail.com>
2019-01-26 13:27:12 -0500

    working on ticker

--- Ticker/Ticker.ino
+++ Ticker/Ticker.ino
@@ -1,3 +1,4 @@
+//#define FASTLED_ALLOW_INTERRUPTS 0  //may help with flickering , may cause other headaches https://github.com/FastLED/FastLED/issues/306
 #include <FastLED.h>
 #include <ESP8266WiFi.h>
 #include <LEDMatrix.h>
@@ -11,6 +12,9 @@
 //https://github.com/AaronLiddiment/LEDText/wiki
 //Set BOARD to NodeMCU1.0
 
+//FLICKERING
+//https://www.youtube.com/watch?v=s5yLLtPrKAM&feature=youtu.be&t=308
+
 const char *ssid = "TruffleShuffle";
 const char *password = "Mets1234";
 
@@ -18,29 +22,37 @@ WiFiClient espClient;
 PubSubClient mqttclient(espClient);
 
 // Change the next 6 defines to match your matrix type and size
-
 #define LED_PIN        7  //GPIO13
 #define COLOR_ORDER    GRB
 #define CHIPSET        WS2812B
-
 #define MATRIX_WIDTH   60
 #define MATRIX_HEIGHT  7
 #define MATRIX_TYPE    HORIZONTAL_ZIGZAG_MATRIX
 
 #define MAXMSG 5
 
-cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> leds;
+#define MODE_SCOREBOARD 1
+#define MODE_NEWS 2
 
+cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> leds;
 cLEDText ScrollingMsg;
 
-char TxtDemo[] = { "          Hello!         "};
-
+//BOOKEEPING
+int hasWifi = 1;
+int isScrollingText = 0;
 String msgList[MAXMSG];
 char * msgStr;
-
 int currentMsgIdx=0;
 int totalMsg=0;
 int nextSlot = 0;
+
+//SCOREBOARD
+int GAMEMAX_LIST[] = {11, 15, 21, 50};
+int gameMaxIdx = 0;
+int currentBlueScore = -1;
+int currentRedScore = -1;
+
+int mode = MODE_SCOREBOARD;
 
 void setup()
 {
@@ -52,18 +64,21 @@ void setup()
   WiFi.begin ( ssid, password );
   delay(500);
   FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds[0], leds.Size());
-  
-  FastLED.setBrightness(50); //64
+  FastLED.setBrightness(25); //64
   FastLED.clear(true);
   delay(100);
   int i = 0;
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 250);
+ while ( WiFi.status() != WL_CONNECTED ) {
+    delay (250);
     leds[0][i] = CRGB::Red;
     i++;
     FastLED.show();
+    if (i>40){
+      hasWifi = 0;
+    }
   }
- //connectToMQTT();
+  connectToMQTT();
+
   dbg("clearing");
   FastLED.clear(true);
 
@@ -72,43 +87,179 @@ void setup()
   nextSlot=0;
 
   dbg("initializing text");
-  //ScrollingMsg.SetFont(RobotronFontData);
-  ScrollingMsg.SetFont(MatriseFontData);
+  ScrollingMsg.SetFont(RobotronFontData);
+  //ScrollingMsg.SetFont(MatriseFontData);
   ScrollingMsg.Init(&leds, leds.Width(), ScrollingMsg.FontHeight() + 1, 0, 0);
-  ScrollingMsg.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
-  ScrollingMsg.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0xff, 0x00, 0x00);
+  if (hasWifi==0){
+    scrollText("   NO WIFI!     WELCOME ");
+  } else {
+    scrollText("     WELCOME APAP'S ");
+  }
 }
+
+//BUTTON HANDLERS
+
+void blueBtnLongPressed(){
+  if (mode == MODE_SCOREBOARD){
+    if (currentBlueScore>0)
+      currentBlueScore--;
+    checkGame();
+  }
+}
+
+void redBtnLongPressed(){
+  if (mode == MODE_SCOREBOARD){
+    if (currentBlueScore>0)
+      currentRedScore--;
+    checkGame();
+  }
+}
+
+void blueBtnPressed(){
+  if (mode == MODE_SCOREBOARD){
+    currentBlueScore++;
+    checkGame();
+  }
+}
+
+void redBtnPressed(){
+  if (mode == MODE_SCOREBOARD){
+    currentRedScore++;
+    checkGame();
+  }
+}
+
+void menuBtnPressed(){
+  if (mode == MODE_SCOREBOARD){
+    if (hasScore){
+      //nothing
+    } else {
+      dbg("change max score");
+      gameMaxIdx++;
+      int arrLen = sizeof(GAMEMAX_LIST) / sizeof(GAMEMAX_LIST[0]);
+      if (gameMaxIdx >= arrLen){
+        gameMaxIdx = 0;
+      }
+    }
+  }
+}
+
+void menuBtnLongPressed(){
+  if (mode == MODE_SCOREBOARD){
+    if (hasScore){
+      dbg("resetting score");
+      startGame();
+    } else {
+      dbg("switch mode");
+    }
+  }
+}
+
+// ***********SCOREBOARD STUFF***********
+
+bool hasScore(){
+  if (currentBlueScore > 0 || currentRedScore >0)
+    return true;
+  else 
+    return false;
+}
+
+void checkGame(){
+  showScore(currentBlueScore,currentRedScore);
+  //TODO 
+}
+
+void startGame(){
+  dbg("Start game");
+  scrollText("      GAME to "+GAMEMAX_LIST[gameMaxIdx]);
+  while (isScrollingText==1){
+    
+  }
+  currentBlueScore=0;
+  currentRedScore=0;
+  checkGame();
+}
+
+
+void showScore(int blue, int red){
+  char blueScore[2];
+  blueScore[0] = (int)'0'+blue / 10;
+  blueScore[1] = (int)'0'+blue % 10;
+  char redScore[2];
+  redScore[0] = (int)'0'+red / 10;
+  redScore[1] = (int)'0'+red % 10;
+  char tmp[]= {'\xe0','\x01','\x01','\xFF',' ',blueScore[0],blueScore[1],'-','\xe0','\xFF','\x01','\x01',redScore[0],redScore[1],'\0'};
+  ScrollingMsg.SetFont(RobotronFontData); 
+  showText(tmp);
+}
+
+/* *********** THE LOOP *********** */
 
 void loop(){
-  dbg("LOOP");
-  char x = '\x00';
-  char tmp[] = {EFFECT_RGB "\x01\x01\xFF 5 -" EFFECT_RGB  "\xFF\x01\x01 5"};  //cant have x00 messes up strings
-  showText(tmp);
-  delay(5000);
-  char tmp2[] = {EFFECT_RGB "\x01\x01\xFF 6 -" EFFECT_RGB  "\xFF\x01\x01 5"};
-  showText(tmp2);
-  delay(3000);
-  char tmp3[] = {EFFECT_RGB "\x01\x01\xFF 6 -" EFFECT_RGB  "\xFF\x01\x01 6"};
-  showText(tmp3);
-  delay(2000);
+  //dbg("LOOP");
+  if (isScrollingText == 0){
+    if (mode == MODE_SCOREBOARD){
+      //dbg("mode is scoreboard");
+      if(currentBlueScore < 0){
+        startGame();
+      }
+    } else {
+      //dbg("Mode not supported "+mode);    //test this
+      showText("Error 1x"+mode);
+    }
+  }
+  //handle any scrolling text
+  
+  if (isScrollingText == 1){
+    if (ScrollingMsg.UpdateText() == -1){
+      isScrollingText= 0;
+      dbg("scrolling complete");
+    }
+    FastLED.show();
+  } 
 }
 
+
+//LED UTILS
 void showText(String msg){
   FastLED.clear(true);
   int len = msg.length();
   char * mStr = (char *)malloc(len+1);
-  msg.toCharArray(mStr,len+1);
+  msg.toCharArray(mStr,len+1);      //malloc with no free, is this a problem?
   mStr[len]='\0';
   ScrollingMsg.SetText((unsigned char *)mStr, len);
   ScrollingMsg.UpdateText();
   FastLED.show();
 }
 
+void scrollText(String msg){
+  ScrollingMsg.SetFrameRate(3);
+  ScrollingMsg.SetFont(MatriseFontData);
+  //ScrollingMsg.SetFont(RobotronFontData);
+  //ScrollingMsg.SetScrollDirection(SCROLL_UP);
+  ScrollingMsg.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0xff, 0x00, 0x00);
+  FastLED.clear(true);
+  int len = msg.length();
+  char * mStr = (char *)malloc(len+1);  //malloc with no free, is this a problem?
+  msg.toCharArray(mStr,len+1);
+  mStr[len]='\0';
+  ScrollingMsg.SetText((unsigned char *)mStr, len);
+  isScrollingText = 1;
+  while (ScrollingMsg.UpdateText() != -1){
+      dbg("scrolling");
+      FastLED.show();
+  }
+  dbg("scrolling complete");
+  isScrollingText = 0;   
+}
+
+/*
 void loop2()
 {
   if (ScrollingMsg.UpdateText() == -1){
-    if (msgStr)
+    if (msgStr){
       free(msgStr);
+    }
     mqttclient.loop();
     delay(150);
     currentMsgIdx++;
@@ -116,17 +267,12 @@ void loop2()
     if (currentMsgIdx >= max){
       currentMsgIdx = 0;
     }
+    
     dbg("Displaying msgidx "+String(currentMsgIdx));
     int len = msgList[currentMsgIdx].length();
-   // char textPtr[len+1];
     msgStr = (char *)malloc(len+1);
     dbg("Length of msg: "+String(len));
   
-    /*msgList[currentMsgIdx].toCharArray(textPtr,len+1);
-    textPtr[len]='\0';
-    dbg("length of textptr"+String(strlen(textPtr)));
-    dbg("Setting text to ");
-    dbg(String(textPtr));*/
 
     msgList[currentMsgIdx].toCharArray(msgStr,len+1);
     msgStr[len]='\0';
@@ -135,28 +281,14 @@ void loop2()
    
  
   }
-  else
+  else{
     FastLED.show();
+  }
   delay(10);
  
 }
 
-void connectToMQTT(){
-  mqttclient.setServer("pizero1", 1883);
- 
-  String clientId = "TICKER-";
-  clientId += String(random(0xffff), HEX);
-  if (mqttclient.connect(clientId.c_str())) {
-      dbg("Connected to MQTT");
-      regServer();
-      mqttclient.setCallback(msgrcvd);
-      mqttclient.subscribe("ticker/#");
-  } else {
-      dbg("mqtt failed, rc=");
-      dbg(String(mqttclient.state()));   
-  }
-  
-}
+*/
 
 void msgrcvd(char* topic, byte* payload, unsigned int length) {
   /* DONT MAKE NETWORK CALLS HERE */
@@ -174,12 +306,32 @@ void msgrcvd(char* topic, byte* payload, unsigned int length) {
     if (nextSlot >= MAXMSG){
       nextSlot = 0;
     }
+  } else if (topicStr == "ticker/button"){
+    dbg("btn click "+msg);
   }
 }
 
 void dbg(String s){
   Serial.println(s);
   Serial.flush();
+}
+
+
+void connectToMQTT(){
+  mqttclient.setServer("pizero1", 1883);
+ 
+  String clientId = "TICKER-";
+  clientId += String(random(0xffff), HEX);
+  if (mqttclient.connect(clientId.c_str())) {
+      dbg("Connected to MQTT");
+      regServer();
+      mqttclient.setCallback(msgrcvd);
+      mqttclient.subscribe("ticker/#");
+  } else {
+      dbg("mqtt failed, rc=");
+      dbg(String(mqttclient.state()));   
+  }
+  
 }
 
 void regServer(){
